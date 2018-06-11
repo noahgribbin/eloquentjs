27bcb3570618f2739dadbc8312fd513176b5c79e
Noah Gribbin <noahgribin@yahoo.com>
2018-06-11 10:45:56 -0700

    build and sum array

---
+++ 04/sum-range.js
@@ -0,0 +1,28 @@
+function range(start, end, step){
+  let returnArr = [];
+    if(start<end){
+        for(let i=start; i <= end; i += step || 1){
+            returnArr.push(i)
+        }
+    }else{
+        for(let i=start; i >= end; i += step){
+            returnArr.push(i)
+        }
+    }
+  return returnArr
+}
+
+function sum(arr){
+    let count = 0;
+    while(arr.length){
+        count += arr.shift()
+    }
+  return count;
+}
+
+console.log(range(1, 10));
+// → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(range(5, 2, -1));
+// → [5, 4, 3, 2]
+console.log(sum(range(1, 10)));
+// → 55
